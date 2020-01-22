900b1b99614484d3e6bb3254cc8179ac5e4adea4
bashiqali111 <60100484+bashiqali111@users.noreply.github.com>
2020-01-22 14:13:59 +0530

    Update email.php

--- email.php
+++ email.php
@@ -4,7 +4,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-$to = "adamgoredesign@gmail.com";
+$to = "bashiqali111@gmail.com";
 
 mail ($to, $subject, $message, "From: " . $name);
 
